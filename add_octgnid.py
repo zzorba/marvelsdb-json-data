7fe997e671da091feba35ed0aabd98d50d37afcc
twistedsistem <twistedsistem@gmail.com>
2020-11-10 09:46:25 -0500

    Added octgn_ids and fixed a couple Red Skull card numbers

---
+++ add_octgnid.py
@@ -0,0 +1,28 @@
+import json
+import uuid
+
+runFile = 'trors_encounter.json'
+
+with open('./pack/' + runFile) as json_file:
+    data = json.load(json_file)
+    updated_data = data.copy()
+    for item in updated_data:
+        try:
+            if 'octgn_id' not in item.keys():
+                item['octgn_id'] = str(uuid.uuid4())
+        except KeyError:
+            print("An exception occurred: " + item['name'])
+
+    for item in updated_data:
+        try:
+            if len(item['code']) > 5 and str(item['code'])[4:5] != 'a':
+                for items in updated_data:
+                    if items['code'] == str(item['code'])[0:5] + 'a':
+                        item['octgn_id'] = items['octgn_id']
+        except KeyError:
+            print("An exception occurred: " + item['name'])
+
+
+with open('./pack/' + runFile, 'w') as outfile:
+    json.dump(updated_data, outfile, indent=4, sort_keys=True)
+        
