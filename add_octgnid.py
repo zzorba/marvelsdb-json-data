290d26f577bc8bdafa41cd1603eeffe2b7e9f11c
Jordan Weiler <jordan.weiler@emberex.com>
2025-04-19 23:08:52 -0700

    Add remaining non-encounter cards

--- add_octgnid.py
+++ add_octgnid.py
@@ -1,8 +1,8 @@
 import json
 import uuid
 
-runFile = 'bp.json'
-pack_code = 'bp'
+runFile = 'aos.json'
+pack_code = 'aos'
 
 with open('./packs.json') as json_file:
     pack_data = json.load(json_file)
